ba1a95f409c30d372024e01f882f380dfd823911
zflovcoding <2043166212@qq.com>
2021-12-01 20:34:58 +0800

    completed second part of lab1

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -1,5 +1,5 @@
 /** Class that determines whether or not a year is a leap year.
- *  @author YOUR NAME HERE
+ *  @author FENG ZHU
  */
 public class LeapYear {
 
@@ -13,7 +13,14 @@ public class LeapYear {
             System.out.printf("%d is not a leap year.\n", year);
         }
     }
+    public static boolean isLeapYear(int year){
+        if((year%400==0) || (year%4==0&&year%100!=0)){
+            return true;
+        }else{
+            return false;
+        }
 
+    }
     /** Must be provided an integer as a command line argument ARGS. */
     public static void main(String[] args) {
         if (args.length < 1) {
